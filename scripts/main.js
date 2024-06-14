387fe997b0a7c18e0320056d67fac081e66b837f
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-14 05:26:05 +0200

    feat(createElementWithClass): function that create an element ewith an assigned class created

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-
+// Create an element with a class
+function createElementWithClass(elementTag , className) {
+    const element = document.createElement(elementTag);
+    element.classList.add(className);
+    return element;
+}
