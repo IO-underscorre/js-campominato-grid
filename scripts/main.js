5b31a9a9ba0bcc7c8a60a8a5844eb83f565077ac
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-14 05:28:54 +0200

    feat(createLandfield): function to add sectors to the minefield container completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,22 @@
+// Append a number of landfield sector's div to a minefield contrainer
+function createLandfield (minefieldElement , numberOfSectors) {
+    const numberOfRowsAndColumns = Math.sqrt(numberOfSectors);
+    minefieldElement.style.gridTemplate = `repeat(${numberOfRowsAndColumns} , 1fr) / repeat(${numberOfRowsAndColumns} , 1fr)`;
+
+    minefield.innerHTML = '';
+    
+    for (i = 0 ; i < numberOfSectors ; i++) {
+        minefieldSector = createElementWithClass('div' , 'minefield-sector');
+
+        minefieldSector.addEventListener('click' , function () {
+            this.classList.add('checked');
+        });
+
+        minefieldElement.append(minefieldSector);
+    }
+}
+
+
 // Create an element with a class
 function createElementWithClass(elementTag , className) {
     const element = document.createElement(elementTag);
